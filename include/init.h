6dbb0823c3d22393290b88610d1e3216723512b4
Karl Sutt <karl@sutt.ee>
2012-10-14 22:20:08 +0200

    Update include/init.h
    Fix typo in macro.

--- include/init.h
+++ include/init.h
@@ -1,4 +1,3 @@
-
 /*
  * Framework for allowing functions to register setup calls for kernel
  * boot.
@@ -9,7 +8,7 @@ extern unsigned int _initcall_end;
 
 typedef void (*initcall_fn)(void);
 
-#define init_call(fn) initcall_fn __initcal_##fn	\
+#define init_call(fn) initcall_fn __initcall_##fn	\
 	__attribute__((section(".initcall"))) = fn
 
 void init();
